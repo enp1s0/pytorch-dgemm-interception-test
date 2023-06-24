b647d307e7b1c66d92308d07bcfd784facea5249
mutsuki <mutsuki@momo86.net>
2023-06-24 12:53:40 +0900

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,6 @@
 //
 // extern "C" void dgemm_(char *transa, char *transb, int *m, int *n, int *k, double *alpha, const double *a, int *lda, const double *b, int *ldb, double *beta, double *c, int *ldc);
 
-void dgemm_core(
-		char *transa, char *transb, int *m, int *n, int *k, double *alpha, const double *a, int *lda, const double *b, int *ldb, double *beta, double *c, int *ldc
-		) {
-	std::printf("DGEMM(%d, %d, %d) is called\n", *m, *n, *k);
-}
-
 extern "C" void dgemm_(char *transa, char *transb, int *m, int *n, int *k, double *alpha, const double *a, int *lda, const double *b, int *ldb, double *beta, double *c, int *ldc) {
-	dgemm_core(transa, transb, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc);
+	std::printf("DGEMM(%d, %d, %d) is called\n", *m, *n, *k);
 }
